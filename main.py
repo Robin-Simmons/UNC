90819c7a493eee6c81d0f9760938fd8286618e5f
Robin <rs17751@bristol.ac.uk>
2020-12-07 19:08:32 +0000

    Started MC Tree search. Rollout finished

--- main.py
+++ main.py
@@ -55,7 +55,6 @@ class game:
         print("{}'s move.".format(player_str(self.player)))
         move_legal = False
         while move_legal == False:
-            
             if np.all(np.isnan(self.last_move)) == True:
                 #if free move, let player chose from the free subgames
                 free_game = False
@@ -74,24 +73,24 @@ class game:
             move_legal = self.check_move_legal(sub_game, move)
         self.board[int(sub_game[0]*3 + move[0]), int(sub_game[1]*3 + move[1])] = self.player
         return move, sub_game
-    
+           
     def iterate_game(self):
         move, sub_game = self.make_move()
         #checks sub board thats just been played to see if it has been won or drawn
         sub_board = self.board[sub_game[0]*3:sub_game[0]*3+3, sub_game[1]*3:sub_game[1]*3+3]
-        
         #updates meta board with with any new wins/draws
         board_win_state = self.check_game_won(sub_board)
         self.meta_board[sub_game[0],sub_game[1]] = board_win_state
         # if the subgame was drawn or won, set the next move to be a free one
         if board_win_state != 0:
-            sub_game = np.array([np.nan, np.nan])
+            move = np.array([np.nan, np.nan])
+            
         #checks if the total game has been won/drawn
         game_win_state = self.check_game_won(self.meta_board)
         if game_win_state == self.player:
             print("Player {} wins".format(player_str(self.player)))
             return False
-        elif game_win_state == np.nan:
+        elif np.isnan(game_win_state) == True:
             print("Game is draw")
             game_sum = np.sum(self.meta_board)
             if game_sum > 0:
@@ -107,82 +106,48 @@ class game:
            self.last_move = move
            return True
        
-"""        
-class game_state:
-    def __init__(self):
-        
-        self.board = np.zeros([9,9])
-        self.meta_board = np.zeros([3,3])
-        self.last_move = np.array([np.nan, np.nan]) #last_move being nan means
-        self.current_player = 1
-        # any sub game can be choosen
-    def check_board(self, board, player):
-        
-        diagonal = np.zeros([3,3])
-        for i in range(3):
-            diagonal[i,i] = 1
-        
-        diagonal_reverse = np.flip(diagonal, axis = 0)
-        if np.array_equal(board, player*diagonal) == True or np.array_equal(board, player*diagonal_reverse) == True:
-            return True 
-        for k in range(3):
-                        
-            if np.array_equal(board[k,:], player*np.ones(3)) == True or np.array_equal(board[:,k], player*np.ones(3)) == True:
-                return True
-        return False
+    def iterate_rollout(self, move, sub_game):
     
-    def check_win(self):
-        if 0 not in self.meta_board:
-            print("DRAW")
-        for player in [-1,1]:
-            for i in range(3):
-                for j in range(3):
-                    sub_game = self.board[i*3:i*3+3,j*3:j*3+3]
-                    if self.check_board(sub_game, player) == True and self.meta_board[i,j] == 0:
-                        self.last_move = np.array([np.nan, np.nan])
-                        self.meta_board[i,j] = player
-                    
-    def check_move_legal(self, sub_game, move):
-        # numpy nans evaluate to True 
-        
-        if self.board[int(sub_game[0]*3 + move[0]), int(sub_game[1]*3 + move[1])] == 0:
-            print("legal move")
-        else:
-            print("illegal move")
+        #checks sub board thats just been played to see if it has been won or drawn
+        sub_board = self.board[sub_game[0]*3:sub_game[0]*3+3, sub_game[1]*3:sub_game[1]*3+3]
+        #updates meta board with with any new wins/draws
+        board_win_state = self.check_game_won(sub_board)
+        self.meta_board[sub_game[0],sub_game[1]] = board_win_state
+        # if the subgame was drawn or won, set the next move to be a free one
+        if board_win_state != 0:
+            move = np.array([np.nan, np.nan])
             
-    def make_move(self):
-        if self.current_player == 1:
-            print("X's move")
+        #checks if the total game has been won/drawn
+        game_win_state = self.check_game_won(self.meta_board)
+        if game_win_state == self.player:
+            return False, self.player
+        elif np.isnan(game_win_state) == True:
+            return False, 0
         else:
-            print("O's move")
-        # moves are in format (i,j,k,l), with ij refering to which sub game
-        # and k,l refering to pos within chosen sub game 
-        print(self.last_move)
-        if np.all(np.isnan(self.last_move)) == True:
-            sub_game = np.fromstring(input("Sub game: "), sep = ",")
-            
-        else:
-            sub_game = self.last_move
-        move = np.fromstring(input("Move within sub game {}: ".format(sub_game)), sep = ",")
-        self.check_move_legal(sub_game, move)
-        self.board[int(sub_game[0]*3 + move[0]), int(sub_game[1]*3 + move[1])] = self.current_player
-        self.last_move = move
-        self.check_win()
-        self.current_player = -1*self.current_player
-"""        
-        
+           #iterate the player and last move
+           self.player = -1*self.player
+           self.last_move = move
+           return True, 0
+
 def player_str(player):
     if player == 0:
         return " "
     elif player == 1:
         return "X"
+    elif player == "-":
+
+        return "-"
+    
     else:
         return "O"
     
     
-def drawboard(board):
+def drawboard(game):
+    board = game.board
+    
     x_win = np.array([[1,0,1],[0,1,0],[1,0,1]])
     o_win = np.array([[-1,-1,-1],[-1,0,-1],[-1,-1,-1]])
+   
     for i in range(9):
         if i == 3 or i == 6:
             print("============#===========#============")
@@ -191,12 +156,14 @@ def drawboard(board):
             print("------------#-----------#------------")
         substring = ""
         for j in range(9):
-            
             if game.meta_board[i//3,j//3] == 1:
                 
                 fill = x_win[i%3,j%3]
             elif game.meta_board[i//3,j//3] == -1:
                 fill = o_win[i%3,j%3]
+            elif np.isnan(game.meta_board[i//3,j//3]) == True:
+                
+                fill = "-"
             else:
                 fill = board[i,j]
             fill = player_str(fill)
@@ -215,11 +182,4 @@ def drawboard(board):
         print(substring + "#")
         
     print("------------#-----------#------------")
-# game loop
-game = game()
-drawboard(game.board)
-game_cont  = True
-while game_cont == True:
-    game_cont = game.iterate_game()
-    print(game.meta_board)
-    drawboard(game.board)
+    
---
+++ mc_tree_search.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  7 00:30:37 2020
+
+@author: Robin
+"""
+import copy
+import time
+
+import numpy as np
+import main
+from anytree import Node, RenderTree
+#0.0022916666666666667
+#upper confidence bound for node
+def UCB(V,N,n):
+    if n == 0:
+        return np.inf
+    else:
+        return V + 2*np.sqrt(np.log(N)/n)
+
+def rollout(roll_node):
+    #simulate game starting from leaf node state (roll node)
+    rollout_game = main.game()
+    rollout_game.board = roll_node.state
+    sub_game = np.fromstring(roll_node.name, sep = ",").astype(int)
+    #if subgame has been won/drawn already, randomly chose a new subgame
+    
+   
+    game_cont = True
+    i = 0
+    while game_cont == True:
+        if rollout_game.meta_board[sub_game[0], sub_game[1]] != 0:
+            allowed_games  = np.argwhere(rollout_game.meta_board == 0)
+            sub_game = allowed_games[np.random.choice(allowed_games.shape[0])]
+        sub_board = rollout_game.board[sub_game[0]*3:sub_game[0]*3+3, sub_game[1]*3:sub_game[1]*3+3]
+        #creates list of allowed moves that the monte carlo search can choose
+        allowed_moves  = np.argwhere(sub_board == 0)
+        #randomly chooses move (seems a bit hacky, must be a better way...)
+        move = allowed_moves[np.random.choice(allowed_moves.shape[0])]
+        rollout_game.board[sub_game[0]*3+move[0], sub_game[1]*3+move[1]] = rollout_game.player
+        game_cont, winner = rollout_game.iterate_rollout(move, sub_game)
+        sub_game = move
+        
+    print(winner)
+    return winner
+
+
+def create_tree(game):
+    #root is the current game state
+    root = Node("root", parent=None, t = 0, n = 0, state = copy.deepcopy(game.board))
+    
+    #create a child for each possible move (9-filled squares)
+    for i in range(3):
+        for j in range(3):
+            board = copy.deepcopy(game.board)
+            last_move = game.last_move
+            if game.check_move_legal(last_move, [i,j]) == True:
+                board[int(last_move[0]*3 + i), int(last_move[1]*3 + j)] = -1
+                Node("{},{}".format(i,j), parent = root, t = 0, n = 0, state = board)
+    #set ucb for all leaf nodes
+    ucb = np.ones(len(root.children))*np.inf
+    for i in range(40):
+        arg = np.argmax(ucb)
+        roll_node = copy.deepcopy(root.children[arg])
+        
+        score = rollout(roll_node)
+        root.children[arg].t = score
+        root.children[arg].n += 1
+        root.children[arg].parent.n += 1
+        for ancestor in root.children[arg].ancestors:
+            ancestor.t = score
+        ucb[arg] = UCB(root.children[arg].t, root.children[arg].n, root.children[arg].parent.n)
+        
+    print(ucb)
+#X is player
+#O is AI
+#X starts
+game = main.game()
+main.drawboard(game)
+for i in range(2):
+    if game.player == 1:
+        game.iterate_game()
+        main.drawboard(game)
+    else:
+        create_tree(game)
+    
